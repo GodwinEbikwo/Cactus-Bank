fc40445dff80b566f63372358936122a94bbc449
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2020-07-30 08:58:14 +0100

    new new

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeEls = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("clicked");
+
+  if (header.classList.contains("open")) {
+    //close hamburger menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeEls.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    //open hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeEls.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
